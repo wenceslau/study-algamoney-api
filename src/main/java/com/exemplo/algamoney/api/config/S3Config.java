e3e6ae7b7bee98cc3395e5b0cb80515864f05d00
Wenceslau <wbaneto@ymail.com>
2018-05-09 09:19:12 -0300

    Updates

--- src/main/java/com/exemplo/algamoney/api/config/S3Config.java
+++ src/main/java/com/exemplo/algamoney/api/config/S3Config.java
@@ -1,11 +1,13 @@
 package com.exemplo.algamoney.api.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.BucketLifecycleConfiguration;
@@ -21,12 +23,16 @@ public class S3Config {
 	@Autowired
 	private AlgamoneyApiProperty property;
 
+	@Bean
 	public AmazonS3 amazonS3() {
 		AWSCredentials credenciais = new BasicAWSCredentials(property.getS3().getAccessKeyId(),
 				property.getS3().getSecretAccessKey());
 
 		AmazonS3 amazonS3 = AmazonS3ClientBuilder.standard()
-				.withCredentials(new AWSStaticCredentialsProvider(credenciais)).build();
+				.withCredentials(new AWSStaticCredentialsProvider(credenciais))
+				.withRegion(Regions.US_EAST_1)
+				.build();
+
 
 		if (!amazonS3.doesBucketExistV2(property.getS3().getBucket())) {
 			amazonS3.createBucket(new CreateBucketRequest(property.getS3().getBucket()));
--- src/main/java/com/exemplo/algamoney/api/resource/LancamentoResource.java
+++ src/main/java/com/exemplo/algamoney/api/resource/LancamentoResource.java
@@ -1,8 +1,6 @@
 package com.exemplo.algamoney.api.resource;
 
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
@@ -35,6 +33,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.exemplo.algamoney.api.dto.Anexo;
 import com.exemplo.algamoney.api.dto.LancamentoEstatisticaCategoria;
 import com.exemplo.algamoney.api.dto.LancamentoEstatisticaDia;
 import com.exemplo.algamoney.api.event.RecursoCriadoEvent;
@@ -45,6 +44,7 @@ import com.exemplo.algamoney.api.repository.filter.LancamentoFilter;
 import com.exemplo.algamoney.api.repository.projection.ResumoLancamento;
 import com.exemplo.algamoney.api.service.LancamentoService;
 import com.exemplo.algamoney.api.service.exception.PessoaInexistenteOuInativoExcpetion;
+import com.exemplo.algamoney.api.storage.S3;
 
 @RestController
 @RequestMapping("/lancamentos")
@@ -62,28 +62,28 @@ public class LancamentoResource {
 	@Autowired
 	private MessageSource messageSource;
 
-	
+	@Autowired
+	private S3 s3;
+
 	@PostMapping("/anexo")
 	@PreAuthorize("hasAuthority('ROLE_CADASTRAR_LANCAMENTO') and #oauth2.hasScope('write')")
-	public String uploadAnexo(@RequestParam MultipartFile anexo) throws IOException {
-		OutputStream out = new FileOutputStream(
-				"/home/alexandre/Desktop/anexo--" + anexo.getOriginalFilename());
-		out.write(anexo.getBytes());
-		out.close();
-		return "ok";
+	public Anexo uploadAnexo(@RequestParam MultipartFile anexo) throws IOException {
+//		OutputStream out = new FileOutputStream("/home/alexandre/Desktop/anexo--" + anexo.getOriginalFilename());
+//		out.write(anexo.getBytes());
+//		out.close();
+		String nome = s3.salvarTeporariamente(anexo);
+		return new Anexo(nome, s3.configurarUrl(nome));
 	}
-	
+
 	@GetMapping("/relatorio/por-pessoa")
 	@PreAuthorize("hasAuthority('ROLE_PESQUISAR_LANCAMENTO') and #oauth2.hasScope('read')")
 	public ResponseEntity<byte[]> relatorioPorPessoa(
-			@RequestParam @DateTimeFormat(pattern="yyyy-MM-dd") LocalDate inicio, 
-			@RequestParam @DateTimeFormat(pattern="yyyy-MM-dd")  LocalDate fim) throws Exception{
+			@RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate inicio,
+			@RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate fim) throws Exception {
 
-		byte[] relatorio = lancamentoService.relatorioPorPessoa(inicio, fim);	
-		
-		return ResponseEntity.ok()
-				.header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_PDF_VALUE)
-				.body(relatorio);
+		byte[] relatorio = lancamentoService.relatorioPorPessoa(inicio, fim);
+
+		return ResponseEntity.ok().header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_PDF_VALUE).body(relatorio);
 	}
 
 	@GetMapping("/estatistica/por-categoria")
